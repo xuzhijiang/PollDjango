fb12c6cf28b50459c013cdf759e8ff270ec20f29
Popovich <cutecowboy@yeah.net>
2018-05-12 15:32:23 +0800

    learn django vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.template import RequestContext, loader
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 
 
 '''
@@ -38,8 +39,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = 'You\'re looking at the result of question %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    q = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = q.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            'question': q,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
