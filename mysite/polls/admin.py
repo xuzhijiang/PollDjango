d75d36452a691c50579054da9bb4cc11308055a2
Popovich <cutecowboy@yeah.net>
2018-05-16 11:27:17 +0800

    custom management form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Question
 
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Data information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
